f86f418174aab6fe3aeada61affe21e73953135f
Tote <103387801+Tote1i@users.noreply.github.com>
2022-04-14 16:18:58 -0300

    Soma de Dois números

---
+++ Aula1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a,b;
+    printf("Sum of two numbers\n");
+    printf("\nEnter the first number: ");
+    scanf("%d",&a);
+    printf("Enter the second number: ");
+    scanf("%d",&b);
+    printf("\n");
+    printf("The sum of %d with %d é %d",a,b,a + b);
+    return 0;
+}
