e22a2eb5d1d1c728bb0701c4dd14a8e9e967f16c
Tote <103387801+Tote1i@users.noreply.github.com>
2022-05-24 10:16:50 -0300

    Create Aula224.c
    Operação push.
    Como empilhar um dado na estrutura de dados pilha

---
+++ Estrutura_de_dados_pilha/Aula224.c
@@ -0,0 +1,80 @@
+//Curso de Programação C | Operação push. Como empilhar um dado na estrutura de dados pilha | aula 224
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+        Estruturas de dados dinâmicos
+*/
+
+typedef struct{
+    int dia,mes,ano;
+}Data;
+
+typedef struct {
+    char nome [50];
+    Data data;
+}Pessoa;
+
+typedef struct no{
+    Pessoa p;
+    struct no *proximo; //Ponteiro para o proximo no
+}No;
+
+Pessoa ler_pessoa() {
+    Pessoa p;
+
+    printf("\nDigite o nome e data de nascimento dd mm aaaa:\n");
+    scanf("%49[^\n]%d%d%d", p.nome, &p.data.dia, &p.data.mes, &p.data.ano);
+    return p;
+}
+
+void imprimir_pessoa(Pessoa p) {
+    printf("\nNome: %s\nData: %2d/%2d/%4d\n", p.nome, p.data.dia, p.data.mes, p.data.ano);
+}
+
+//Função para a operação PUSH (empilhar)
+No* empilhar (No*topo) {
+    No *novo = malloc(sizeof(No));
+
+    //verificar se a alocação de memoria funcionou
+    if(novo) {
+        novo->p = ler_pessoa();
+        novo->proximo = topo;
+        return novo;
+    }
+    else
+        printf("\nErro ao alocar a memoria\n");
+    return NULL;
+}
+
+int main () {
+
+    No *topo = NULL; //pilha
+    int opcao;
+
+    do {
+        printf("\n0 - Sair\n1 - Empilhar\n2 - Desempilhar\n3 - Imprimir\n");
+        scanf("%d", &opcao);
+        getchar(); //limpar buffer do teclado
+
+        switch (opcao)
+        {
+        case 1:
+            topo = empilhar(topo);
+            break;
+        case 2:
+            /* code */
+            break;  
+        case 3:
+            /* code */
+            break;              
+        
+        default:
+            if(opcao !=0)
+                printf("\nOpcao invalida\n");
+            
+        }
+    } while(opcao !=0);
+    return 0;
+}
